b44d1fe469f8bc5d99540f7c51deee59ee298774
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-25 08:32:17 +0530

    Backend''

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const server = require('http').createServer();
+let express  = require('express');
+let app = express();
+let port =process.env.PORT||1000;
+app.get('/',(req,res)=>{
+    res.send("your name is nitin and this is your frist server side");
+});
+app.listen(port,()=>{
+    console.log(`....${port}`);
+});
+console.log(process.env.port);
